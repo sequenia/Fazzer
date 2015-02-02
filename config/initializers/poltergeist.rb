da3366ceb8a53fa4d204e5eedecc4d11663da4b7
4ybakut2004 <4ybakut2004@gmail.com>
2015-02-02 16:01:10 +0600

    Удалил ненужые логи

--- config/initializers/poltergeist.rb
+++ config/initializers/poltergeist.rb
@@ -5,14 +5,12 @@ require "capybara/poltergeist/utility"
 module Capybara::Poltergeist
   Client.class_eval do
     def start
-      puts '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
       @pid = Process.spawn(*command.map(&:to_s), pgroup: true)
       ObjectSpace.define_finalizer(self, self.class.process_killer(@pid))
     end
  
     def stop
       if pid
-        puts 'STOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOP!'
         kill_phantomjs
         ObjectSpace.undefine_finalizer(self)
       end
