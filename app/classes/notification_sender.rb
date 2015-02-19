e84204199bdcb9941d4e6e531a300961cbc9eb81
4ybakut2004 <4ybakut2004@gmail.com>
2015-02-19 17:30:46 +0600

    Произвольные сообщения через нотификации

--- app/classes/notification_sender.rb
+++ app/classes/notification_sender.rb
@@ -7,12 +7,32 @@ class NotificationSender
 	#   badge: 5,     # Количество сообщений, отображаемое в уведомлении
 	#   sound: 'name' # Звук оповещения
 	# }
-	def self.send(platform, token, message, params)
+	#def self.send(platform, token, message, params)
+	#	if platform == "android"
+	#		gcm = GCM.new(ENV["GCM_API_KEY"])
+	#		registration_ids = [token]
+	#		options = {data: {message: message}, collapse_key: "updated_score"}
+	#		response = gcm.send(registration_ids, options)
+	#	elsif platform == "ios"
+	#	end
+	#end
+
+	# options = {
+	#   data: JSON,         # JSON для отправления на мобильное устройство
+	#   collapse_key: "Key" # Назначение сообщения
+	# }
+	def self.send(platform, token, options)
+		options ||= {}
 		if platform == "android"
 			gcm = GCM.new(ENV["GCM_API_KEY"])
+
 			registration_ids = [token]
-			options = {data: {message: message}, collapse_key: "updated_score"}
-			response = gcm.send(registration_ids, options)
+
+			gcm_options = {}
+			gcm_options[:data]         = options[:data]         if options[:data]
+			gcm_options[:collapse_key] = options[:collapse_key] if options[:collapse_key]
+
+			response = gcm.send(registration_ids, gcm_options)
 		elsif platform == "ios"
 		end
 	end
