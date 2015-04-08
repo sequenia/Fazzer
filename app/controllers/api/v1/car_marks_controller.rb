197e1def17b1289a8486a05caec60f0c52722b0c
4ybakut2004 <4ybakut2004@gmail.com>
2015-04-08 16:33:42 +0600

    Выгрузка марок авто

--- app/controllers/api/v1/car_marks_controller.rb
+++ app/controllers/api/v1/car_marks_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CarMarksController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: :version
 
   respond_to :json
 
